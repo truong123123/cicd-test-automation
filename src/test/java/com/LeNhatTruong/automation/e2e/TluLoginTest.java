31d20797478a0fcb7fb45a85d48c0bfc41d6c09b
LeNhatTruong <truongng15112005@gmail.com>
2026-06-10 14:23:38 +0700

    testt

--- src/test/java/com/LeNhatTruong/automation/e2e/TluLoginTest.java
+++ src/test/java/com/LeNhatTruong/automation/e2e/TluLoginTest.java
@@ -49,7 +49,7 @@ public class TluLoginTest {
     @Test
     public void testLoginAccount1() {
         System.out.println("Running login test 1...");
-        performLogin("2351067118", "077205009742");
+        performLogin("2351067118", "077205009740");
     }
 
     @Test
